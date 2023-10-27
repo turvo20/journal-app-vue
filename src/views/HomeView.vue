e9fd2112ab8afd3f33efbbd2a7fda06ada830824
Rudy Casilla <turvo200@gmail.com>
2023-10-27 16:10:44 -0400

    agregando sass al proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-   
+
   </div>
+  <button class="btn btn-primary">
+    primary
+   </button>
+   <button class="btn btn-secondary">
+    secundary
+   </button>
+   <button class="btn btn-success">
+    success
+   </button>
 </template>
 
